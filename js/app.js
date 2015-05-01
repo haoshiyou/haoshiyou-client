09d61c0487a0aa82b9e1413686f73d46e96ff6ff
Zainan Victor Zhou <xinbenlv@gmail.com>
2015-04-30 19:00:46 -0700

    Big refactor, removed index3.html

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 jQuery(function ($) {
-    $(document).ready(function () {
-        var template = $("#value_panel_template").clone();
-        var parent = $("#value_panel_parent").empty();
-        var DATA_KEY = "GoogleSheetData-3.1";
+    "use strict";
 
-        Parse.initialize("RPjt04MBMl3rzzEKBGKUpP7KHFXAsomtDbr9cS0y", "DSqWg9xElC6mI1PwtPWzzeIPQYUXPGTOC66nT82h");
+    var CONST_LOCALSTORAGE_KEY = "GoogleSheetData-3.1";
+    var CONST_PARSE_ID = "RPjt04MBMl3rzzEKBGKUpP7KHFXAsomtDbr9cS0y";
+    var CONST_PARSE_KEY = "DSqWg9xElC6mI1PwtPWzzeIPQYUXPGTOC66nT82h";
+    var CONST_SPREADSHEET_URL = "https://spreadsheets.google.com/feeds/list/1vzugrYLpgXmFODqysSx331Lhq8LpDQGJ4sQtwSMrtV4/1/public/values?alt=json";
+
+    var CONST_COLUMNS = {
+        xq: "gsx$需求类别",
+        qssj: "gsx$预计起始日期超过这个日期本条信息失效",
+        qy: "gsx$区域",
+        ybhzcs: "gsx$邮编或者城市",
+        yqjg: "gsx$预期价格美元每月",
+        grjs: "gsx$租房需求介绍和自我介绍",
+        wxId: "gsx$微信号",
+        xb: "gsx$性别",
+        dhyx: "gsx$其他联系方式电话邮箱等",
+        fbsj: "gsx$timestamp",
+        ltst: "gsx$shorttermorlongterm",
+        entryid: "gsx$entryid"
+    };
+
+
+    function init() {
+
+        Parse.initialize(CONST_PARSE_ID, CONST_PARSE_KEY);
         zHelper.trackerFunctions["woopra"] = function(action, dimensions) {
             woopra.track(action, dimensions);
         };
@@ -14,6 +34,7 @@ jQuery(function ($) {
         zHelper.trackerFunctions["ga"] = function(action, dimensions) {
             ga('send', 'event', 'button', 'click', action, dimensions);
         };
+
         zHelper.trackerFunctions["parse"] = function(action, dimensions) {
             var ParseTracking = Parse.Object.extend("ParseTracking");
             var parseTracking = new ParseTracking();
@@ -38,10 +59,10 @@ jQuery(function ($) {
         zHelper.track("view");
 
         $.getJSON("http://www.telize.com/geoip?callback=?",
-            function (jsonp) {
-                zHelper.geoInfo.ip = jsonp.ip;
-                zHelper.geoInfo.city = jsonp.city;
-                zHelper.geoInfo.country = jsonp.country;
+            function (data) {
+                zHelper.geoInfo.ip = data.ip;
+                zHelper.geoInfo.city = data.city;
+                zHelper.geoInfo.country = data.country;
                 zHelper.track("loaded-geo-info");
             }
         );
@@ -54,136 +75,138 @@ jQuery(function ($) {
                 //options.url = "http://cors.corsproxy.io/url=" + options.url;
             }
         });
-        var url = "https://spreadsheets.google.com/feeds/list/1vzugrYLpgXmFODqysSx331Lhq8LpDQGJ4sQtwSMrtV4/1/public/values?alt=json";
-        var columns = {
-            xq: "gsx$需求类别",
-            qssj: "gsx$预计起始日期超过这个日期本条信息失效",
-            qy: "gsx$区域",
+    }
 
-            ybhzcs: "gsx$邮编或者城市",
-            yqjg: "gsx$预期价格美元每月",
-            grjs: "gsx$租房需求介绍和自我介绍",
-            wxId: "gsx$微信号",
-            xb: "gsx$性别",
-            dhyx: "gsx$其他联系方式电话邮箱等",
-            fbsj: "gsx$timestamp",
-            ltst: "gsx$shorttermorlongterm",
-            entryid: "gsx$entryid"
-        };
 
-        var refreshPage = function(data){
-            var pageRow = parseInt($.url().param("row"));
-            var getCell = function(row, col) {
-                try {
-                    var cell = data.feed.entry[row][columns[col]]["$t"] || "N/A";
-                    return cell;
-                } catch(e) {
-                    zHelper.log("error when getting cell", "ERROR", {e: e, row: row, col: col, data: data});
-                }
-            };
-            var createChild = function(row) {
-                var child = template.clone();
-                var xq = getCell(row, "xq");
-                child.find("#value_wxId").hide();
-                child.find("#value_dhyx").hide();
-                for (var col in columns) {
-                    child.find("#value_" + col).text(getCell(row, col));
+    /**
+     * From our spreadsheet, get a cell data.
+     * @param row a row number
+     * @param col A string as the column key
+     * @returns {*|string}
+     */
+    function getCell(row, col, data) {
+        console.assert(data !== undefined);
+        try {
+            var cell = data.feed.entry[row][CONST_COLUMNS[col]]["$t"] || "N/A";
+            return cell;
+        } catch(e) {
+            zHelper.log("error when getting cell", "ERROR", {e: e, row: row, col: col, data: data});
+        }
+    };
 
-                }
-                if (xq == "出租") {
-                    child.addClass("panel-success");
-                } else if (xq == "求租") {
-                    child.addClass("panel-primary");
-                } else if (xq == "找室友") {
-                    child.addClass("panel-warning");
-                }
-                child.find("#value_wxId").hide();
-                child.find("#btn_show_wxId").click(function(){
-                    child.find("#btn_show_wxId").hide();
-                    child.find("#value_wxId").show();
-                    zHelper.track("show-wechat-id", {pageRow: pageRow});
-                });
-                child.find("#btn_show_dhyx").click(function(){
-                    child.find("#btn_show_dhyx").hide();
-                    child.find("#value_dhyx").show();
-                    zHelper.track("show-dhyx", {pageRow: pageRow});
-                });
-                var rowLink = $.url().attr("path") + "?" + $.param({"row": row});
-                child.find("#row_link").attr("href", rowLink);
-                child.find("#btn_share").attr("data-row", row);
-                child.find("#btn_like").attr("data-row", row);
-                child.show();
-                return child;
-            };
-            if (!isNaN(pageRow)) {
-                var child = createChild(pageRow);
-                parent.append(child);
-                var rowLink = $.url().attr("source");
-                zHelper.log("custom sharing " + rowLink);
-                weixin.wxData.link = rowLink;
-                var title =  "求分享：" +getCell(pageRow, "xq") + "-" + getCell(pageRow, "wz");
-                weixin.wxData.title = title;
-                weixin.wxData.desc = title + ",起始时间:" + getCell(pageRow, "qssj") + ", 预期价格" + getCell(pageRow, "yqjg");
-                weixin.wxCallbacks.confirm = function() {
-                    zHelper.log("分享成功", "INFO", rowLink);
-                    $.notify("分享成功", "success");
-                };
-                weixin.wxCallbacks.fail = function() {
-                    zHelper.log("分享失败", "WARNING", rowLink);
-                    $.notify("分享失败", "danger");
-                };
-            } else {
-                var dataLength = data.feed.entry.length;
-                for (var i = dataLength - 1; i >= 0; i--) {
-                    var child = createChild(i);
-                    parent.append(child);
-                }
-                var homeLink = $.url().attr("source");
-                zHelper.log("custom sharing " + homeLink);
-                weixin.wxData.link = rowLink;
-                var title =  "朋友找房子你愿意帮忙介绍吗？";
-                weixin.wxData.title = title;
-                weixin.wxData.desc = title + "湾区租房互助群，靠谱租房靠朋友。rent.zzn.im";
-                weixin.wxCallbacks.confirm = function() {
-                    zHelper.log("分享成功", "INFO", rowLink);
-                    $.notify("分享成功", "success");
-                };
-                weixin.wxCallbacks.fail = function() {
-                    zHelper.log("分享失败", "WARNING", rowLink);
-                    $.notify("分享失败", "danger");
-                };
-            }
-        };
-        var row = 1;
-        parent.empty();
+    
+    function createChild(row, data) {
+        console.assert(data !== undefined);
+        var template = $("#value_panel_template").clone();
+        var child = template.clone();
+        var xq = getCell(row, "xq", data);
+        child.find("#value_wxId").hide();
+        child.find("#value_dhyx").hide();
+        for (var col in CONST_COLUMNS) {
+            child.find("#value_" + col).text(getCell(row, col, data));
+
+        }
+        if (xq == "出租") {
+            child.addClass("panel-success");
+        } else if (xq == "求租") {
+            child.addClass("panel-primary");
+        } else if (xq == "找室友") {
+            child.addClass("panel-warning");
+        }
+        child.find("#value_wxId").hide();
+        child.find("#btn_show_wxId").click(function(){
+            child.find("#btn_show_wxId").hide();
+            child.find("#value_wxId").show();
+            zHelper.track("show-wechat-id", {pageRow: pageRow});
+        });
+        child.find("#btn_show_dhyx").click(function(){
+            child.find("#btn_show_dhyx").hide();
+            child.find("#value_dhyx").show();
+            zHelper.track("show-dhyx", {pageRow: pageRow});
+        });
+        var rowLink = $.url().attr("path") + "?" + $.param({"row": row});
+        child.find("#row_link").attr("href", rowLink);
+        child.find("#btn_share").attr("data-row", row);
+        child.find("#btn_like").attr("data-row", row);
+        child.attr("id", "value_panel" + getCell(row, "entryid", data));
+        child.show();
+
+        return child;
+    };
+
+
+    var refreshPage = function(data){
         $("#loading_panel").show();
-        var cachedData = JSON.parse(localStorage.getItem(DATA_KEY));
+        var parent = $("#value_panel_parent");
+        parent.empty();
+        var pageRow = parseInt($.url().param("row"));
+        if (!isNaN(pageRow)) {
+            var child = createChild(pageRow, data);
+            parent.append(child);
+            var rowLink = $.url().attr("source");
+            zHelper.log("custom sharing " + rowLink);
+            weixin.wxData.link = rowLink;
+            var title =  "求分享：" +
+                getCell(pageRow, "xq", data) + "-" +
+                getCell(pageRow, "wz", data);
+            weixin.wxData.title = title;
+            weixin.wxData.desc = title + ",起始时间:" +
+                getCell(pageRow, "qssj", data) + ", 预期价格" +
+                getCell(pageRow, "yqjg", data);
+            weixin.wxCallbacks.confirm = function() {
+                zHelper.log("分享成功", "INFO", rowLink);
+                $.notify("分享成功", "success");
+            };
+            weixin.wxCallbacks.fail = function() {
+                zHelper.log("分享失败", "WARNING", rowLink);
+                $.notify("分享失败", "danger");
+            };
+        } else {
+            var dataLength = data.feed.entry.length;
+            for (var i = dataLength - 1; i >= 0; i--) {
+                var child = createChild(i, data);
+                parent.append(child);
+            }
+            var homeLink = $.url().attr("source");
+            zHelper.log("custom sharing " + homeLink);
+            weixin.wxData.link = rowLink;
+            var title =  "朋友找房子你愿意帮忙介绍吗？";
+            weixin.wxData.title = title;
+            weixin.wxData.desc = title + "湾区租房互助群，靠谱租房靠朋友。rent.zzn.im";
+            weixin.wxCallbacks.confirm = function() {
+                zHelper.log("分享成功", "INFO", rowLink);
+                $.notify("分享成功", "success");
+            };
+            weixin.wxCallbacks.fail = function() {
+                zHelper.log("分享失败", "WARNING", rowLink);
+                $.notify("分享失败", "danger");
+            };
+        }
+
+        $("#loading_panel").hide();
+    };
+
+    /**
+     *
+     * @param cb A function taking data as input and display that data.
+     */
+    function loadData(cb) {
+        var cachedData = JSON.parse(localStorage.getItem(CONST_LOCALSTORAGE_KEY));
         var alwaysRefresh = true; // TODO(zzn): consider how should I configure caching.
         if (alwaysRefresh || typeof cachedData === "undefined" || cachedData == null) {
             zHelper.log("cache missed, loading data...");
-            $.get(url, function(data) {
+            $.get(CONST_SPREADSHEET_URL, function (data) {
                 zHelper.log("loaded data from CORS proxy");
-                localStorage.setItem(DATA_KEY, JSON.stringify(data));
-                refreshPage(data);
+                localStorage.setItem(CONST_LOCALSTORAGE_KEY, JSON.stringify(data));
+                cb(data);
             });
         } else {
             zHelper.log("using cached data");
-            refreshPage(cachedData);
+            cb(cachedData);
         }
-        $("#loading_panel").hide();
-        $("#refresh_btn").click(function() {
-            zHelper.log("force fetch new data");
-            parent.empty();
-            $("#loading_panel").show();
-            $.get(url, function(data) {
-                zHelper.log("loaded data from CORS proxy");
-                localStorage.setItem(DATA_KEY, JSON.stringify(data));
-                setTimeout(function(){
-                    refreshPage(data);
-                    $("#loading_panel").hide();
-                }, 250);
-            });
-        });
+    }
+
+    function setFooterButtons() {
         $(".zzn-panel-footer-button").click(function(e){
             var action = $(this).attr("data-track-action");
             var row = $(this).attr("data-row");
@@ -192,5 +215,15 @@ jQuery(function ($) {
             $('#notReadyModal').find("#feature").text(action);
             $('#notReadyModal').modal();
         });
+    };
+
+    function main() {
+        init();
+        loadData(refreshPage);
+        setFooterButtons();
+    };
+
+    $(document).ready(function () {
+        main();
     });
 });
