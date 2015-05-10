3185ca8e4cc8e42fc2ab5ff968deefac548e3e18
Zainan Victor Zhou <xinbenlv@gmail.com>
2015-05-09 18:12:24 -0700

    Update wechat sharing

--- js/weixin.js
+++ js/weixin.js
@@ -14,26 +14,26 @@ var weixin = (function(zHelper, wx) {
         // 分享操作开始之前
         ready: function () {
             // 你可以在这里对分享的数据进行重组
-            zHelper.log("weixin ready");
+            zHelper.log("weixin ready","INFO" ,resp);
         },
         // 分享被用户自动取消
         cancel: function (resp) {
-            zHelper.log("weixin cancel");
+            zHelper.log("weixin cancel","INFO" ,resp);
             // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
         },
         // 分享失败了
         fail: function (resp) {
-            zHelper.log("weixin fail");
+            zHelper.log("weixin fail","INFO" ,resp);
             // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
         },
         // 分享成功
         confirm: function (resp) {
-            zHelper.log("weixin confirm");
+            zHelper.log("weixin confirm","INFO" ,resp);
             // 分享成功了，我们是不是可以做一些分享统计呢？
         },
         // 整个分享过程结束
         all: function (resp) {
-            zHelper.log("weixin all");
+            zHelper.log("weixin all", "INFO" ,resp);
             // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
         }
     };
@@ -43,7 +43,8 @@ var weixin = (function(zHelper, wx) {
         imgUrl: wxData.imgUrl, // 分享图标
         desc: wxData.desc,
         success: wxCallbacks.confirm,
-        cancel: wxCallbacks.cancel
+        cancel: wxCallbacks.cancel,
+        fail: wxCallbacks.fail
     };
     wx.onMenuShareTimeline(dataAndCallBack);
     wx.onMenuShareAppMessage(dataAndCallBack);
