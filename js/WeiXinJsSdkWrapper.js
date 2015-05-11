af18c54c96c94ef8fa6a65764871104830255d7d
Zainan Victor Zhou <xinbenlv@gmail.com>
2015-05-10 21:50:42 -0700

    Update wechat sharing

--- js/WeiXinJsSdkWrapper.js
+++ js/WeiXinJsSdkWrapper.js
@@ -95,6 +95,7 @@ var WeiXinJsSdkWrapper = (function($, Hashes, wx, zHelper, ParseJsGlobalCache){
             console.log(msg);
             console.log("XXX signature!");
             console.log(signature);
+            alert("XXX before wx.config ready");
             wx.config({
                 debug: zHelper.isDebug(), // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: CONST_APPID, // 必填，公众号的唯一标识
@@ -106,10 +107,11 @@ var WeiXinJsSdkWrapper = (function($, Hashes, wx, zHelper, ParseJsGlobalCache){
                     "onMenuShareAppMessage"
                 ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
+            alert("XXX before ready");
             wx.ready(function(res){
                 zHelper.log("Wechat Config OK!");
-                alert("wx.ready");
-                alert(res);
+                alert("XXX wx.ready");
+                alert(res); //XXX
                 wx.checkJsApi({
                     jsApiList: [
                         "onMenuShareTimeline",
