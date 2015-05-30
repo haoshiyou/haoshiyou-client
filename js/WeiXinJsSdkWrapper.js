1700af6643ad79a68f4211d94e6d374d8de8dd5f
Zainan Victor Zhou <xinbenlv@gmail.com>
2015-05-30 00:10:01 -0700

    Update

--- js/WeiXinJsSdkWrapper.js
+++ js/WeiXinJsSdkWrapper.js
@@ -102,7 +102,7 @@ var WeiXinJsSdkWrapper = (function($, Hashes, wx, zHelper, ParseJsGlobalCache){
             zHelper.log("XXX before wx.config ready");
             zHelper.logAlert("000 wx=" + JSON.stringify(wx));
             wx.config({
-                debug: zHelper.isDebug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: zHelper.isDebug(), // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: CONST_APPID, // 必填，公众号的唯一标识
                 timestamp: timestamp, // 必填，生成签名的时间戳
                 nonceStr:nonceStr, // 必填，生成签名的随机串
