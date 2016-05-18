a4cbfd29e463d06ca95be5b0fcde52c26f610b92
Zainan Victor Zhou <xinbenlv@gmail.com>
2016-05-18 13:08:44 -0700

    add cordova google map plugin

--- lab/haoshiyou/app/pages/page2/page2.ts
+++ lab/haoshiyou/app/pages/page2/page2.ts
@@ -1,11 +1,23 @@
 import {Page} from 'ionic-angular';
+import { OnInit } from 'angular2/core';
 
+declare let window: any;
 
 @Page({
   templateUrl: 'build/pages/page2/page2.html',
 })
-export class Page2 {
+export class Page2 implements OnInit {
+
   constructor() {
 
   }
+
+  ngOnInit() {
+    var div = document.getElementById("map_canvas");
+
+    // Initialize the map view
+    let map: any = window.plugin.google.maps.Map.getMap(div);
+  }
+
+
 }
