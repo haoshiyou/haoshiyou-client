2375b27628def822ef45acc59786d8c72eb213ae
Zainan Victor Zhou <xinbenlv@gmail.com>
2016-05-19 20:58:36 -0700

    Update

--- lab/haoshiyou/app/pages/listings-tab/listing-detail.ts
+++ lab/haoshiyou/app/pages/listings-tab/listing-detail.ts
@@ -1,12 +1,9 @@
 import {Page, NavParams} from "ionic-angular";
-import {Listing, ListingId} from "../../listing";
-import {EnumMsgPipe} from "../../enum-msg.pipe";
-import {ListingService, MockListingService} from "../../listing.service";
-import {provide} from "angular2/core";
+import {Listing} from "../../listing";
+import {EnumMsgPipe} from "../../enum-msg.pipe";]
 
 @Page({
   templateUrl: 'build/pages/listings-tab/listing-detail.html',
-  providers:[ provide(ListingService, {useClass: MockListingService})],
   pipes: [EnumMsgPipe]
 })
 export class ListingDetailPage {
@@ -14,19 +11,9 @@ export class ListingDetailPage {
 
   /**
    * Based on params or id load a listing page.
-   * @param listingService
    * @param params
-   * @param id
    */
-  constructor(private listingService:ListingService,
-              params?:NavParams, id?:ListingId) {
-
-    if (params && params.data && params.data.listing) {
+  constructor(params:NavParams) {
       this.listing = params.data.listing;
-    } else if (id) {
-      listingService.getListingById(id).then(listing => this.listing = listing);
-    } else {
-      throw "No valid listing passed in";
-    }
   }
 }
