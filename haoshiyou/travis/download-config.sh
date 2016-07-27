f45c22cca97abfe6c358894004c30471d6ca2bec
Zainan Victor Zhou <xinbenlv@gmail.com>
2016-07-27 06:58:01 -0700

    Add mkdir to travis download script

--- haoshiyou/travis/download-config.sh
+++ haoshiyou/travis/download-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 set -ev
+mkdir -p www/config
 curl -H 'Authorization: token $HAOSHIYOU_SECURITY_TOKEN' -H 'Accept: application/vnd.github.v3.raw' -o www/config/config.json -O -L https://api.github.com/repos/xinbenlv/haoshiyou-security/config.json
