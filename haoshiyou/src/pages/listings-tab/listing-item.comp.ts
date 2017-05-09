a20aa0371047941c6804c3d0b7bc976d5e484040
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-05-09 08:39:16 -0700

    Add ga

--- haoshiyou/src/pages/listings-tab/listing-item.comp.ts
+++ haoshiyou/src/pages/listings-tab/listing-item.comp.ts
@@ -2,6 +2,7 @@ import {Component, Input} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {ListingDetailPage} from "./listing-detail.page";
 import {HsyListing} from "../../loopbacksdk/models/HsyListing";
+declare let ga:any;
 
 @Component({
   selector: 'listing-item',
@@ -14,6 +15,7 @@ export class ListingItem {
   }
 
   gotoDetail() {
+    ga('send', 'view-listing-detail');
     this.nav.push(ListingDetailPage, {listing: this.listing});
   }
 }
