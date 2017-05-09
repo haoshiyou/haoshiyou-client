54e06a5af1183c5d1a0863e294c3491aa1fba053
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-05-09 12:44:46 -0700

    Show Version

--- haoshiyou/src/pages/settings-tab/settings-tab.page.ts
+++ haoshiyou/src/pages/settings-tab/settings-tab.page.ts
@@ -1,13 +1,29 @@
 import {AuthService} from "../../services/auth.service";
-import {Component} from "@angular/core";
+import {Component, AfterViewInit} from "@angular/core";
 import {Env} from "../../app/env";
+import {CodePush} from "@ionic-native/code-push";
+import {Platform} from "ionic-angular";
 
 @Component({
   templateUrl: 'settings-tab.page.html'
 })
-export class SettingsTabPage {
-  constructor(public auth:AuthService) {
+export class SettingsTabPage implements AfterViewInit {
 
+  async ngAfterViewInit() {
+    await this.platform.ready();
+    if (this.platform.is('cordova')) {
+      let pInfo = await this.codePush.getCurrentPackage();
+      this.version = `CodePush:${pInfo.appVersion}`;
+    } else {
+      this.version = `Non-Cordova:` + Env.version;
+    }
   }
-  public version = Env.version;
+
+  constructor(
+      public auth:AuthService,
+      private codePush:CodePush,
+      private platform:Platform) {
+  }
+
+  public version:string = Env.version;
 }
