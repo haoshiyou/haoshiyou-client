88ba96c56e6fc287bd12aae5b2cabfab1c2c062e
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-05-09 23:23:06 -0700

    Update to show versions

--- haoshiyou/src/pages/settings-tab/settings-tab.page.ts
+++ haoshiyou/src/pages/settings-tab/settings-tab.page.ts
@@ -1,23 +1,19 @@
 import {AuthService} from "../../services/auth.service";
-import {Component, AfterViewInit} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Env} from "../../app/env";
 import {CodePush} from "@ionic-native/code-push";
 import {Platform} from "ionic-angular";
+declare let window:any;
 
 @Component({
   templateUrl: 'settings-tab.page.html'
 })
-export class SettingsTabPage implements AfterViewInit {
+export class SettingsTabPage implements OnInit {
 
-  async ngAfterViewInit() {
-    await this.platform.ready();
-    if (this.platform.is('cordova')) {
-      let pInfo = await this.codePush.getCurrentPackage();
-      this.version = `CodePush:${pInfo.appVersion}`;
-    } else {
-      this.version = `Non-Cordova:` + Env.version;
-    }
-  }
+  public versionApp:string = Env.version;
+  public versionDownloaded:string = null;
+  public versionPending:string = null;
+  public versionRemote:string = null;
 
   constructor(
       public auth:AuthService,
@@ -25,5 +21,24 @@ export class SettingsTabPage implements AfterViewInit {
       private platform:Platform) {
   }
 
-  public version:string = Env.version;
+  public async startSync() {
+    this.codePush.sync().subscribe((syncStatus) => console.log(syncStatus));
+    let downloadProgress = (progress) => { console.log(`Downloaded ${progress.receivedBytes} of ${progress.totalBytes}`); }
+    this.codePush.sync({}, downloadProgress).subscribe((syncStatus) => console.log(syncStatus));
+    await this.updateVersions();
+  }
+  public async updateVersions() {
+    if (this.platform.is('cordova')) {
+      await this.platform.ready();
+      let currentPackageInfo = await this.codePush.getCurrentPackage();
+      this.versionDownloaded = currentPackageInfo ? currentPackageInfo.appVersion :  `未知`;
+      let pendingPackageInfo = await this.codePush.getPendingPackage();
+      this.versionPending = pendingPackageInfo ? pendingPackageInfo.appVersion :  `未知`;
+      let remotePackageInfo = await this.codePush.checkForUpdate();
+      this.versionRemote = remotePackageInfo ? remotePackageInfo.appVersion + remotePackageInfo.downloadUrl :  `未知`;
+    }
+  }
+  async ngOnInit() {
+    await this.updateVersions();
+  }
 }
