9226be5313fb54f67350d3d52f50252d07f3e021
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-05-28 13:19:43 +0800

    Show actual date in lastUpdated field

--- haoshiyou/src/app/app.module.ts
+++ haoshiyou/src/app/app.module.ts
@@ -44,6 +44,7 @@ import {Network} from "@ionic-native/network";
 import {Push} from "@ionic-native/push";
 import {CodePush} from "@ionic-native/code-push";
 import {AppVersion} from "@ionic-native/app-version";
+import {DateFormatterPipe} from "../pipes/date-formatter.pipe";
 
 export function getAuthHttp(http, nativeStorage:NativeStorage) {
   return new AuthHttp(new AuthConfig({
@@ -80,7 +81,8 @@ export function getAuthHttp(http, nativeStorage:NativeStorage) {
     ImageIdsToUrlPipe,
     ImageIdToUrlPipe,
     TimeFromNowPipe,
-    CityNZipPipe
+    CityNZipPipe,
+    DateFormatterPipe,
   ],
   imports: [
     IonicModule.forRoot(HaoshiyouApp, {
