0feda617a15a353d881eb95e197fc76312a1bb51
Zainan Victor Zhou <zzn@google.com>
2018-11-06 23:44:47 -0800

    Add support for modal to show QR code

--- haoshiyou/src/pages/qrcode-tab/qrcode-tab-page.ts
+++ haoshiyou/src/pages/qrcode-tab/qrcode-tab-page.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {NavParams, ViewController} from "ionic-angular";
+import { CookieService } from 'ngx-cookie-service';
 declare let ga:any;
 
 @Component({
@@ -6,6 +8,21 @@ declare let ga:any;
   templateUrl: 'qrcode-tab.page.html',
 })
 export class QrCodeTabPage {
+  constructor(
+      public params: NavParams,
+      private viewCtrl:ViewController,
+      private cookie:CookieService) {
+    console.log(params);
+  }
+
+  async dismiss() {
+    this.viewCtrl.dismiss();
+    let expireDate = new Date();
+    let numberOfDaysToAdd = 7;
+    expireDate.setDate(expireDate.getDate() + numberOfDaysToAdd);
+    this.cookie.set("hasShownQrCodeModal", 'true', expireDate);
+  }
+
   public shouldShowQrCode:boolean = false;
 
   ionViewWillEnter() {
